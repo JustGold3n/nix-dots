da43bc90391626512053b8752ac06b7dbad14dd5
JustGold3n <marek@pavlicek.eu>
2026-06-23 15:06:43 +0200

    neco dal

--- modules/hey.nix
+++ modules/hey.nix
@@ -2,19 +2,24 @@
 #
 # ZSH and Janet are the powerhouses of my dotfiles. This module configures both
 # for my scripting needs (particularly by bin/hey).
-
-{ hey, lib, options, config, pkgs, ... }:
-
+{
+  hey,
+  lib,
+  options,
+  config,
+  pkgs,
+  ...
+}:
 with builtins;
 with lib;
-with hey.lib;
-let cfg = config.hey;
+with hey.lib; let
+  cfg = config.hey;
 
-    janet = pkgs.janet;
-    jpm = mkWrapper pkgs.jpm ''
-      wrapProgram $out/bin/jpm --add-flags '--tree="$JANET_TREE" --binpath="$XDG_BIN_HOME" --headerpath=${janet}/include --libpath=${janet}/lib --ldflags=-L${pkgs.glibc}/lib '
-    '';
-    janetTreeDir = "${config.home.dataDir}/janet/jpm_tree";
+  janet = pkgs.janet;
+  jpm = mkWrapper pkgs.jpm ''
+    wrapProgram $out/bin/jpm --add-flags '--tree="$JANET_TREE" --binpath="$XDG_BIN_HOME" --headerpath=${janet}/include --libpath=${janet}/lib --ldflags=-L${pkgs.glibc}/lib '
+  '';
+  janetTreeDir = "${config.home.dataDir}/janet/jpm_tree";
 in {
   options = with types; {
     hey = {
@@ -61,10 +66,11 @@ in {
 
       export JANET_PATH="${janetTreeDir}/lib"
       export JANET_TREE="${janetTreeDir}"
-      ${pkgs.zsh}/bin/zsh -c "cd '${hey.dir}'; jpm deps"
-      ${pkgs.zsh}/bin/zsh -c "cd '${hey.dir}'; jpm run deploy"
-    '';
 
+      # Appending `|| true` prevents the system switch from failing if info.json is absent
+      ${pkgs.zsh}/bin/zsh -c "cd '${hey.dir}'; jpm deps" || true
+      ${pkgs.zsh}/bin/zsh -c "cd '${hey.dir}'; jpm run deploy" || true
+    '';
     programs.zsh.shellInit = mkBefore ''
       export DOTFILES_HOME="${hey.dir}"
       export fpath=( "${hey.libDir}/zsh" "''${fpath[@]}" )
@@ -75,28 +81,34 @@ in {
       "d %h/.local/share/janet/jpm_tree 755 - - - -"
     ];
 
-    home.dataFile = {
-      # This file is intended as a reference for shell scripts to peek into to
-      # do cheap feature-detection (using `hey vars ...`)
-      "hey/info.json".text = toJSON cfg.info;
-    } //
-    # FIXME: Refactor me?
-    (listToAttrs
-      (flatten
-        (mapAttrsToList
-          (hook: hooks: mapAttrsToList
-            (n: v: nameValuePair
-              (let filename =
-                     if (match "^[0-9]{2}-.+" n) == null
-                     then "50-${n}"
-                     else n;
-               in "hey/hooks.d/${hook}.d/${filename}") {
-                 text = ''
-                   #!/usr/bin/env zsh
-                   ${v}
-                 '';
-                 executable = true;
-               }) hooks)
-          config.hey.hooks)));
+    home.dataFile =
+      {
+        # This file is intended as a reference for shell scripts to peek into to
+        # do cheap feature-detection (using `hey vars ...`)
+        "hey/info.json".text = toJSON cfg.info;
+      }
+      //
+      # FIXME: Refactor me?
+      (listToAttrs
+        (flatten
+          (mapAttrsToList
+            (hook: hooks:
+              mapAttrsToList
+              (n: v:
+                nameValuePair
+                (let
+                  filename =
+                    if (match "^[0-9]{2}-.+" n) == null
+                    then "50-${n}"
+                    else n;
+                in "hey/hooks.d/${hook}.d/${filename}") {
+                  text = ''
+                    #!/usr/bin/env zsh
+                    ${v}
+                  '';
+                  executable = true;
+                })
+              hooks)
+            config.hey.hooks)));
   };
 }
