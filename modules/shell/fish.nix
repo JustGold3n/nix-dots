927d242d562cb077a2e32db81b2cfe4ed00e9ed2
JustGold3n <marek@pavlicek.eu>
2026-06-24 13:41:00 +0200

    someting

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -2,26 +2,25 @@
 
 with lib;
 with hey.lib;
-#let cfg = config.modules.shell.fish;
-#in {
-#  options.modules.shell.fish = with types; {
-#    enable = mkBoolOpt false;
-#
-#    rcInit = mkOpt' lines "" ''
-#      Zsh lines to be written to $XDG_CONFIG_HOME/zsh/extra.zshrc and sourced by
-#      $XDG_CONFIG_HOME/zsh/.zshrc
-#    '';
-#    envInit = mkOpt' lines "" ''
-#      Zsh lines to be written to ${config.home.configDir}/zsh/extra.zshenv and
-#      sourced by $XDG_CONFIG_HOME/zsh/.zshenv
-#    '';
-#
+let cfg = config.modules.shell.fish;
+in {
+  options.modules.shell.fish = with types; {
+    enable = mkBoolOpt false;
+
+ #   rcInit = mkOpt' lines "" ''
+ #     Zsh lines to be written to $XDG_CONFIG_HOME/zsh/extra.zshrc and sourced by
+ #     $XDG_CONFIG_HOME/zsh/.zshrc
+ #   '';
+ #   envInit = mkOpt' lines "" ''
+ #     Zsh lines to be written to ${config.home.configDir}/zsh/extra.zshenv and
+ #     sourced by $XDG_CONFIG_HOME/zsh/.zshenv
+ #   '';
+
 #    rcFiles  = mkOpt (listOf (either str path)) [];
 #    envFiles = mkOpt (listOf (either str path)) [];
-#  };
-#
-#  config = mkIf cfg.enable {
-    programs.fish.enable = true;
+  };
+  config = mkIf cfg.enable {
+   programs.fish.enable = true;
 
     users.defaultUserShell = pkgs.fish;
 
@@ -45,5 +44,5 @@ with hey.lib;
       unzip
       vim
     ];
-
+};
 }
